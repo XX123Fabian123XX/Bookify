1f2b22325123e9171617f57a273a23ae9fd6098a
XX123FabianXX123 <fabian.winkelmann1@gmail.com>
2021-12-12 18:57:11 +0100

    Add user case for creating a book

---
+++ backend/src/useCases/books/createBook.js
@@ -0,0 +1,20 @@
+const {makeBook} = require("../../entities/books/index")
+
+exports.buildCreateBook = function(dbConnection) {
+    return async function(bookInformation) {
+        const book = await makeBook(...bookInformation)
+
+        dbConnection.createBook({
+            title: book.getTitle(),
+            author: book.getAuthor(),
+            datePublished: book.getDatePublished(),
+            linkBookCover:book.getLinkBookCover(),
+            linkBookBack:book.getLinkBookBack(),
+            numberPages: book.getNumberPages(),
+            rating: book.getRating(),
+            genre: book.getGenre(),
+            userReference: book.getUserReference(),
+            createdAt: book.getCreatedAt()
+        })
+    } 
+} 
