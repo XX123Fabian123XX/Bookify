0d36c2c6ebef4a7d21fa7a5e738546b83ae7bc38
XX123FabianXX123 <fabian.winkelmann1@gmail.com>
2021-12-13 19:32:56 +0100

    Create makeFakeUser class and add it in the tests

--- backend/src/entities/users/index.js
+++ backend/src/entities/users/index.js
@@ -1,4 +1,4 @@
 const {buildMakeUser} = require("./users");
 const {isEmailValid, isPasswordValid, encryptPassword} = require("./utils")
 
-exports.makeUser = buildMakeUser(isEmailValid, isPasswordValid, encryptPassword )
+exports.makeUser = buildMakeUser(isEmailValid, isPasswordValid, encryptPassword)
--- backend/src/entities/users/users.test.js
+++ backend/src/entities/users/users.test.js
@@ -1,73 +1,67 @@
-const {makeUser} = require("./index.js")
+const makeFakeUser = require("../../../__test__/__fixtures__/user");
 
 describe("entities: UserEntity",  () => {
-    test("name null", () => {
-        return makeUser({email:"fabian.winkelmann1@gmail.com"}).catch(e => expect(e.message).toBe("You need a name to create a user"))
+    it("must have a name", () => {
+        return makeFakeUser({name:null}).catch(e => expect(e.message).toBe("You need a name to create a user"))
     })   
     
-    test("name is too short", () => {
-        return makeUser({name:"TW", "email":"fabian.winkelmann1@gmail.com"})
+    it("must have a name that is long enough", () => {
+        return makeFakeUser({name:"TW"})
         .catch(e => expect(e.message).toBe("The name needs to be as least 3 characters long"))
     })
-    test("email null", () => {
-        return makeUser({name:"Das ist ein test"})
+    it("must have an email", () => {
+        return makeFakeUser({email:null})
         .catch(e=> expect(e.message).toBe("You need an email to create a user"))
     })
-    test("email invalid 1", () => {
+    it("must have a valid email ", () => {
         
-        return makeUser({name:"Das ist ein test",email:"test"})
+        return makeFakeUser({email:"test"})
         .catch(e => expect(e.message).toBe("You need to enter a valid email"))
     })
     
-    test("email invalid 2", () => {
-        return makeUser({name:"Das ist ein test",email:"123123test@gmail."})
+    it("must have a valid email", () => {
+        return makeFakeUser({email:"123123test@gmail."})
         .catch(e => expect(e.message).toBe("You need to enter a valid email"))
     })
 
-    test("get Name", () => {
-        return makeUser({name:"Fabian Winkelmann", email:"fabian.winkelmann1@gmail.com"})
+    it("must return the name correctly", () => {
+        return makeFakeUser({name:"Fabian Winkelmann"})
         .then(e => expect(e.getName()).toBe("Fabian Winkelmann"))
     })
 
-    test("get Email", () => {
-        makeUser({name:"Fabian Winkelmann", email:"fabian.winkelmann1@gmail.com"})
+    it("must return the email correctly", () => {
+        makeFakeUser({email:"fabian.winkelmann1@gmail.com"})
         .then(e => expect(e.getEmail()).toBe("fabian.winkelmann1@gmail.com"))
     })
 
-    test("password is null", () => {
+    it("must have a password", () => {
 
-        return makeUser({name:"Fabian Winkelmann", 
-        email:"fabian.winkelmann1@gmail.com", passwordConfirm:"test"},createWithPassword=true)
+        return makeFakeUser({password:null},createWithPassword=true)
         .catch(e => expect(e.message).toBe("You need a password to create a user"))
     })
 
-    test("password is invalid 1", () => {
-        return makeUser({name:"Fabian Winkelmann", 
-        email:"fabian.winkelmann1@gmail.com",password:"test",passwordConfirm:"test"},createWithPassword=true)
+    it("must have a valid password", () => {
+        return makeFakeUser({password:"test"},createWithPassword=true)
         .catch(e => expect(e.message).toBe("The password is not valid"))
     })
 
-    test("password confirm is null", () => {
-        return makeUser({name:"Fabian Winkelmann", 
-        email:"fabian.winkelmann1@gmail.com", password:"Test1234:)"},createWithPassword = true)
+    it("must have a password confirm", () => {
+        return makeFakeUser({passwordConfirm:null},createWithPassword = true)
         .catch(e => expect(e.message).toBe("You need to confirm your password"))
     })
 
-    test("password and password confirm have to match", () => {
+    it("must have equal password and equal passwordconfirm", () => {
 
-        return makeUser({name:"Fabian Winkelmann",
-        email:"fabian.winkelmann1@gmail.com", password:"testT1234:)",
-        passwordConfirm:"testT12234:)"}, createWithPassword=true)
+        return makeFakeUser({
+        password:"testT1234:)", passwordConfirm:"testT12234:)"}, createWithPassword=true)
         .catch(e => expect(e.message).toBe("The password and the password confirm do not match"))
     })
     
-    test("password is encrypted", () => {
-        return makeUser({name:"Fabian Winkelmann",
-        email:"fabian.winkelmann1@gmail.com", password:"testT1234:)",
-        passwordConfirm:"testT1234:)"}, createWithPassword=true).
+    it("must have an encrypted password", () => {
+        return makeFakeUser({
+        password:"testT1234:)",
+        passwordConfirm:"testT1234"}, createWithPassword=true).
         then(e => expect(e.getPassword).not.toEqual("testT1234:"))
         
     })
-
-
 })
