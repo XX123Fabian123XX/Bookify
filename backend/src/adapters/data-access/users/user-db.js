8e2ee9ace5e9b7c7df4aa3f2e28146d92ca1b643
XX123FabianXX123 <fabian.winkelmann1@gmail.com>
2021-12-18 07:21:12 +0100

    Create the userdb service

---
+++ backend/src/adapters/data-access/users/user-db.js
@@ -0,0 +1,53 @@
+const userModel = require("./userModel")
+const apiFeatures = require("../apiFeatures");
+const AppError = require("../../controllers/errors/appError");
+
+// TODO: REFACER USER DB AND BOOK DB INTO ONE SERVICE
+
+const makeUserDbConnection = (mongooseObject) => {
+    const User = new userModel(mongooseObject);
+
+    const createUser = async (userInformation) => {
+        const newUser = await new User(userInformation).save()
+        delete newUser["_id"]
+        return newUser.toObject()
+    }
+
+    const getAllUsers = async (query) => {
+        const firstQuery = User.find({})
+        const finalQuery = new apiFeatures(firstQuery, query).filter().sort().paginate().limitFields().query;
+        
+        return (await finalQuery).map(el => el.toObject());
+    }
+
+    const getSingleUser = async(id) => {
+        const singleUser = (await User.find({id},{_id:0}))
+
+        if (!singleUser) throw new AppError(`No user was found with this id ${id}`, 404)
+
+        return singleUser.toObject();
+    }
+
+    const deleteUser = async(id) => {
+        const deleteRes = (await User.deleteOne({id}))
+
+        if (deleteRes.deletedCount == 0) throw new AppError(`No user was found with this id ${id}`)
+    }
+
+    const updateUser = async(id, newUserInformation) => {
+        return (await User.findOneAndUpdate({id}, newUserInformation, {new:true, fields:{_id:0}})).toObject()
+    }
+
+    return {
+        deleteUser,
+        updateUser,
+        getSingleUser,
+        getAllUsers,
+        createUser
+    }
+}
+
+module.exports = makeUserDbConnection;
+
+
+
--- backend/src/adapters/routes/userRouter.js
+++ backend/src/adapters/routes/userRouter.js
@@ -4,8 +4,8 @@ const buildUserRouter = function(router, middleware,mongooseObject) {
     return function() {
         const userController = buildUserController(mongooseObject)
 
-        // router.delete("/:id", middleware(deleteUser));
         router.post("/signup", middleware(userController.signupUser));
+        // router.delete("/:id", middleware(deleteUser));
         // router.patch("/:id", middleware(updateUser));
         // router.patch("/password", middleware(updatePassword))
         // router.patch("/resetPassword", middleware(resetPassword))
